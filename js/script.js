cdcd1e0289a4f80141bcd8f6d145f5516490413f
Paul Morin <pmorin35@gmail.com>
2019-02-06 12:29:43 -0800

    Array, properties, function, css
    Added a  new property to the array. Tweeked HTML and CSS  to reflect new property. Added additional html to printQuote function.
    
    Added comments.

--- js/script.js
+++ js/script.js
@@ -1,95 +1,74 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+// created an array of quotes
 
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended:
-    - Add at least one `year` and/or `citation` property to at least one
-      quote object.
-***/
 let quotes = [
   { quote: "The only way to do great work is to love what you do",
     source: "Steve Jobs",
     citation: "Stanford University Commencement Speach",
-    year: 2005
+    year: 2005,
+    tag: "*work*"
   },
   { quote: "Successful and unsuccessful people do not vary greatly in their abilities. They vary in their desires to reach their potential.",
-    source: "John Maxwell"
+    source: "John Maxwell",
+    tag: "*professional*"
   },
   { quote: "Successful people do what unsuccessful people are not willing to do. Don't wish it were easier; wish you were better.",
-    source: "Jim Rohn"
+    source: "Jim Rohn",
+    tag: "*professional*"
   },
   { quote: "It's better to be a lion for a day than a sheep all your life.",
-    source: "Elizabeth Kenny"
+    source: "Elizabeth Kenny",
+    tag: "*life*"
   },
   { quote: "Throw your dreams into space like a kite, and you do not know what it will bring back, a new life, a new friend, a new love, a new country.",
-    source: "Anais Nin"
+    source: "Anais Nin",
+    tag: "*life*"
+  },
+  {
+    quote: "I don’t want you to overcome fear of failure. I want you to be afraid of failure.",
+    source: "Jocko Willink",
+    citation: "Discipline Equals Freedom",
+    year: 2018,
+    tag: "*professional*"
   }
 ];
 
 
+// the getRandomQuote function generates a random number and returns an object form the array.
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number
-   - use the random number to `return` a random quote object from the
-     `quotes` array.
-***/
-
-
-
-function getRandomQuote(){
+function getRandomQuote(array){
   let randomQuote = [Math.floor(Math.random() * quotes.length)]
     return quotes[randomQuote]
 }
 
 
-/***
-  Create the `printQuote` function to:
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
+// assigns a variable to the object pulled from the array and converts it to an HTML string.
+//the returned string will differ depending on the properties of the object.
+
 function printQuote() {
-  let wisdom = getRandomQuote();
+  let wisdom = getRandomQuote(quotes);
   let message = "";
   if (wisdom.citation){
     message += '<p class="quote">' + wisdom.quote + '.</p>';
-    message += '<p class="source">' + wisdom.source +'<span class="citation">' + wisdom.citation + '</span><span class="year">' + wisdom.year + '</span></p>';
+    message += '<p class="source">' + wisdom.source +
+    '<span class="citation">' + wisdom.citation + '</span>' +
+    '<span class="year">' + wisdom.year + '</span>'+
+    '<span class="tag">'  + wisdom.tag  + '</span></p>';
   } else {
     message += '<p class="quote">' + wisdom.quote + '.</p>';
-    message += '<p class="source">' + wisdom.source + '</p>';
+    message += '<p class="source">' + wisdom.source +
+    '<span class="tag"> ' + wisdom.tag + '</span></p>';
   }
 
   document.getElementById("quote-box").innerHTML = message;
 };
 printQuote();
 
-
-//<p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-
-//<p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
-
 /***
+  This line works so I'm just going to use it.
   When the "Show another quote" button is clicked, the event listener
   below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
+  function.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
